812558d266c0a0e5a6bf24432676198be6e9b967
OthonielBaltazar <2018371011@uteq.edu.mx>
2021-09-24 00:30:57 -0500

    app3

---
+++ App3/App3/ViewModels/NombreCompletoViewModel.cs
@@ -0,0 +1,52 @@
+﻿namespace App3.ViewModels
+{
+    class NombreCompletoViewModel : ViewModelBase
+    {
+        string nombre;
+        string apellido1;
+        string apellido2;
+
+        public string Nombre
+        {
+            get => nombre;
+            set
+            {
+                if (string.Equals(nombre, value)) return;
+                nombre = value;
+                RaisePropertyChanged(nameof(Nombre));
+                RaisePropertyChanged(nameof(NombreCompleto));
+            }
+        }
+
+        public string Apellido1
+        {
+            get => apellido1;
+            set
+            {
+                if (string.Equals(apellido1, value)) return;
+                apellido1 = value;
+                RaisePropertyChanged(nameof(Apellido1));
+                RaisePropertyChanged(nameof(NombreCompleto));
+            }
+        }
+
+        public string Apellido2
+        {
+            get => apellido2;
+            set
+            {
+                if (string.Equals(apellido2, value)) return;
+                apellido2 = value;
+                RaisePropertyChanged(nameof(Apellido2));
+                RaisePropertyChanged(nameof(NombreCompleto));
+            }
+        }
+
+        public string NombreCompleto
+        {
+            get => string.Format("Tu nombre completo es : {0} {1} {2} ", nombre,apellido1, apellido2);
+        }
+
+    }
+}
+
